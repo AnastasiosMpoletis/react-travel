69518882bb8598f3a65c367b1b5c53913575cd3c
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-26 16:25:46 +0300

    L-544. Create AccordionItemContext to store the id and pass it to AccordionTitle and AccordionContent

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,7 +1,23 @@
-export default function AccordionItem({ className, children }) {
+import { createContext, useContext } from "react";
+
+const AccordionItemContext = createContext();
+
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
+  
+    if (!ctx) { // in case it is used by a component that is not wrapped by AccordionContext.Provider
+      throw new Error('AccordionItem-related components must be wrapped by <Accordion.Item>.');
+    }
+  
+    return ctx;
+}
+
+export default function AccordionItem({ id, className, children }) {
   return (
-    <li className={className}>
-      {children}
-    </li>
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>
+        {children}
+      </li>
+    </AccordionItemContext.Provider>
   );
 }
